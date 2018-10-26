9141700b4558db8d0bdbf98764c131fd4f9b91a1
Chanho Jung <skylogin@gmail.com>
2018-10-26 22:57:09 +0900

    first commit

---
+++ monolithic.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const url = require("url");
+const querystring = require("querystring");
+
+const members = require("./monolithinc_members.js");
+const goods = require("./monolithinc_goods.js");
+const purchases = require("./monolithinc_purchases.js");
+
+let server = http
+  .createServer((req, res) => {
+    let method = req.method;
+    let uri = url.parse(req.url, true);
+    let pathname = url.pathname;
+
+    if (method === "POST" || method === "PUT") {
+      let body = "";
+
+      req.on("data", function(data) {
+        body += data;
+      });
+
+      req.on("end", function() {
+        let params;
+        if (req.headers["content-type"] === "application/json") {
+          params = JSON.parse(body);
+        } else {
+          params = querystring.parse(body);
+        }
+
+        onRequest(res, method, pathname, params);
+      });
+    } else {
+      onRequest(res, method, pathname, uri.query);
+    }
+  })
+  .listen(8000);
+
+function onRequest(res, method, pathname, params) {
+  switch (pathname) {
+    case "/members":
+      members.onRequest(res, method, pathname, params, response);
+      break;
+    case "/goods":
+      goods.onRequest(res, method, pathname, params, response);
+      break;
+    case "/purchases":
+      purchases.onRequest(res, method, pathname, params, response);
+      break;
+    default:
+      res.writeHead(404);
+      return res.end();
+  }
+}
+
+function response(res, packet) {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(packet));
+}
