419062741250ff52d7b1d39d1d2f36ba1491aacb
Chanho Jung <skylogin@gmail.com>
2018-10-27 19:51:20 +0900

    add business logic

---
+++ micro/microservice_goods.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const business = require("../monolithic/monolithic_goods.js");
+class goods extends require("./server.js") {
+  constuctor() {
+    super("goods", process.argv[2] ? Number(process.argv[2]) : 9010, [
+      "POST/goods",
+      "GET/goods",
+      "DELETE/goods"
+    ]);
+
+    this.connectToDistributor("127.0.0.1", 9000, data => {
+      console.log("Distributor Notification", data);
+    });
+  }
+
+  onRead(socket, data) {
+    console.log("onRead", socket.remoteAddress, socket.remotePort, data);
+    business.onRequest(
+      socket,
+      data.method,
+      data.uri,
+      data.params,
+      (s, packet) => {
+        socket.write(JSON.stringify(packet) + "¶");
+      }
+    );
+  }
+}
+
+new goods();
